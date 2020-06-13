2316ae0dba6dc22aa05079cde8a1a18baeb25d58
franck-gaspoz <franck.gaspoz@gmail.com>
2020-06-13 20:21:39 +0200

    refactoring set command line processor unstatic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using DotNetConsoleAppToolkit.Component.CommandLine.CommandLineReader;
+﻿using DotNetConsoleAppToolkit.Component.CommandLine;
+using DotNetConsoleAppToolkit.Component.CommandLine.CommandLineReader;
 using System;
-using static DotNetConsoleAppToolkit.Component.CommandLine.CommandLineProcessor;
 
 namespace DotNetConsoleAppToolkitShell
 {
@@ -8,8 +8,8 @@ namespace DotNetConsoleAppToolkitShell
     {
         static void Main(string[] args)
         {
-            var commandLineReader = new CommandLineReader();
-            InitializeCommandProcessor(args,commandLineReader);
+            var commandLineProcessor = new CommandLineProcessor(args);
+            var commandLineReader = new CommandLineReader(commandLineProcessor);
             var returnCode = commandLineReader.ReadCommandLine();
             Environment.Exit(returnCode);
         }
