ea42eaddf1d76fef1d434ec4196cd8498eea1914
franck-gaspoz <franck.gaspoz@gmail.com>
2020-06-13 02:31:13 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DotNetConsoleAppToolkit.Component.CommandLine.CommandLineReader;
+using System;
+using static DotNetConsoleAppToolkit.Component.CommandLine.CommandLineProcessor;
+
+namespace DotNetConsoleAppToolkitShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandLineReader = new CommandLineReader();
+            InitializeCommandProcessor(args,commandLineReader);
+            var returnCode = commandLineReader.ReadCommandLine();
+            Environment.Exit(returnCode);
+        }
+    }
+}
+
